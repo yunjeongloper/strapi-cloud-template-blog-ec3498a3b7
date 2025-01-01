e2bc5d1766fbf121268431a78d40554cca6fb748
veronica <vero911@naver.com>
2025-01-02 00:54:13 +0900

    Resolve filtration findOne relation controller

--- src/api/filtration/controllers/filtration.js
+++ src/api/filtration/controllers/filtration.js
@@ -1,9 +1,45 @@
-'use strict';
+"use strict";
 
 /**
  * filtration controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+// @ts-ignore
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::filtration.filtration');
+module.exports = createCoreController(
+  "api::filtration.filtration",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      // for list
+      // await this.validateQuery(ctx);
+
+      const { id } = ctx.params; // URL의 ID 가져오기
+      const { query } = ctx; // 쿼리 파라미터 가져오기
+
+      // fot list
+      // const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+
+      const data = await strapi
+        .service("api::filtration.filtration")
+        .findOne(id, {
+          ...query,
+          populate: {
+            spirits: true, // 첫 번째 관계 데이터
+            movement: {
+              // 두 번째 관계 데이터
+              populate: "to_storages", // 중첩 관계 데이터
+            },
+          },
+        });
+
+      // 데이터가 없는 경우 404 반환
+      if (!data) {
+        return ctx.notFound("The requested resource does not exist");
+      }
+
+      // 응답 변환 및 반환
+      return this.transformResponse(data);
+    },
+  })
+);
